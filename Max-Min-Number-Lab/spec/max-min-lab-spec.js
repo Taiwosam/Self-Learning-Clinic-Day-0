0d43ecc9985d14153b5e8d13c95d090207be8331
Taiwo <taiwo4u2adore@gmail.com>
2017-02-09 00:19:43 +0100

    Made the existing test suites more robust. Added more test cases

--- Max-Min-Number-Lab/spec/max-min-lab-spec.js
+++ Max-Min-Number-Lab/spec/max-min-lab-spec.js
@@ -17,6 +17,13 @@
         expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
       });
 
+      it('should return [3, 12] for [4, 5, 4, 7, 8, 10, 3, 6, 12]', function() {
+        expect(findMinMax([4, 5, 4, 7, 8, 10, 3, 6, 12])).toEqual([3, 12]);
+      });
+
+      it('should return [0, 1] for [1, 0, 0, 1, 1, 0]', function() {
+        expect(findMinMax([1, 0, 0, 1, 1, 0])).toEqual([0, 1]);
+      });
     });
 
     describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
@@ -25,6 +32,21 @@
         expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
       });
 
+      it('should return [1] for [1, 1, 1, 1]', function () {
+        expect(findMinMax([1, 1, 1, 1])).toEqual([1]);
+      });
+
+      it('should return [10] for [10, 10, 10]', function () {
+        expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
+      });
+
+      it('should return [7] for [7, 7]', function () {
+        expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
+      });
+
+      it('should return [5] for [5, 5, 5, 5, 5, 5]', function () {
+        expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
+      });
     });
 
   });
