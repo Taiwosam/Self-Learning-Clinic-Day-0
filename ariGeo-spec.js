c313946d30de4c7e0e88a217422892b7244f6c7e
Taiwo <taiwo4u2adore@gmail.com>
2017-02-09 04:09:36 +0100

    Additional test cases for the aritGeo function

--- ariGeo-spec.js
+++ ariGeo-spec.js
@@ -26,6 +26,14 @@
         expect(aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
       });
 
+      it("should return `Arithmetic` for [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23]", function() {
+        expect(aritGeo([1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).toEqual('Arithmetic');
+      });
+
+      it("should return `Arithmetic` for [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32]", function() {
+        expect(aritGeo([1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).toEqual('Arithmetic');
+      });
+
     });
 
     describe("Case for a geometric sequence", function() {
@@ -42,6 +50,18 @@
         expect(aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
       });
 
+      it("should return `Geometric` for [4, 8, 16, 32, 64, 128, 256, 512, 1024]", function() {
+        expect(aritGeo([4, 8, 16, 32, 64, 128, 256, 512, 1024])).toEqual('Geometric');
+      });
+
+      it("should return `Geometric` for [4, 8, 16, 32, 64, 128, 256, 512, 1024]", function() {
+        expect(aritGeo([4, 8, 16, 32, 64, 128, 256, 512, 1024])).toEqual('Geometric');
+      });
+
+      it("should return `Geometric` for [5, 10, 20, 40, 80, 160]", function() {
+        expect(aritGeo([5, 10, 20, 40, 80, 160])).toEqual('Geometric');
+      });
+
     });
 
     describe("Case for neither arithmetic nor geometric sequence", function() {
@@ -56,6 +76,14 @@
 
       it("should return -1 for [1, 8, 27, 64, 125]", function() {
         expect(aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
+      });
+
+      it("should return -1 for [2, 1, 5, 4, 6]", function() {
+        expect(aritGeo([2,1, 5, 4, 6])).toEqual(-1);
+      });
+
+      it("should return -1 for [10, 3, 84, 12]", function() {
+        expect(aritGeo([10, 3, 84, 12])).toEqual(-1);
       });
 
     });
