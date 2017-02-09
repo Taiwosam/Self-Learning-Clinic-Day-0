8bbb04cd1973f1dd8b4500e2b4cb4fd522e50727
Taiwo <taiwo4u2adore@gmail.com>
2017-02-09 03:25:14 +0100

    Edited the fizzBuzz function to pass new test suite

--- fizz-buzz-lab/app/fizzBuzz.js
+++ fizz-buzz-lab/app/fizzBuzz.js
@@ -1,4 +1,8 @@
 function fizzBuzz(number) {
+  if (typeof(number) !== 'number'){
+    throw new Error("fizzBuzz only takes numbers");
+  }
+  
   if (number % 5 === 0 && number % 3 === 0) {
     return 'FizzBuzz';
   }
