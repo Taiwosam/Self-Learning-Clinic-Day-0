6068d06441fa775a53ec9fe58e13d4202b5458ba
Taiwo <taiwo4u2adore@gmail.com>
2017-02-09 03:23:26 +0100

    Added test suite to make sure fizzBuzz argument is a number

--- Fizz-Buzz-Lab/spec/Fizz-Buzz-spec.js
+++ Fizz-Buzz-Lab/spec/Fizz-Buzz-spec.js
@@ -1,3 +1,6 @@
+'use strict';
+var fizzBuzz = require('../app/fizzBuzz.js')
+
 describe("Fizz Buzz tests ", function() {
 
   it("should return `Fizz` for number divisible by 3", function() {
@@ -42,6 +45,16 @@ describe("Fizz Buzz tests ", function() {
 
   it("should return `FizzBuzz` for 150", function() {
     expect(fizzBuzz(150)).toBe('FizzBuzz');
+  });
+
+  describe("The fizzBuzz function only takes a number ", function(){
+    it("should return an error for any string", function(){
+      expect(function() { fizzBuzz('taiwo'); }).toThrow(new Error("fizzBuzz only takes numbers"));
+    });
+
+    it("should return an error for a string containing numbers", function(){
+      expect(function() { fizzBuzz('15'); }).toThrow(new Error("fizzBuzz only takes numbers"));
+    });
   });
 
 });
